f542cf314f3287bda1e2477c44ac81eb678b24f2
boredmangetspaid <develop202234@gmail.com>
2025-11-11 23:10:30 -0500

    add todo class

---
+++ src/todo.js
@@ -0,0 +1,9 @@
+class Todo {
+    constructor(name,description,begin,end,completed="incomplete"){
+        this.name=name;
+        this.description= description;
+        this.begin=begin;
+        this.end=end;
+        this.completed=completed;
+    }
+}
