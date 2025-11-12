544b60a7bf05beadc734da9274f15616f6f7e7f0
boredmangetspaid <develop202234@gmail.com>
2025-11-11 23:39:51 -0500

    add id to todo

--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,8 @@
+import {v4 as uuidv4} from "uuid";
+
 class Todo {
     constructor(name,description,begin,end,completed="incomplete"){
+        this.id=uuidv4();
         this.name=name;
         this.description= description;
         this.begin=begin;
